d644408e33c61d0d27193a649dd0f6366d1e3762
Sezen <aysezen6@gmail.com>
2023-04-11 14:43:53 +0200

    V2

--- src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
+++ src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
@@ -2,11 +2,10 @@ package test;
 
 import baseURL.JsonPlaceHolderBaseURL;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseURL {
 
@@ -73,7 +72,17 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseURL {
 
         // 3 - Response'i kaydet
 
+        Response response = given().spec(specJsonPlace).when().get("/{pp1}/{pp2}");
 
+        response.prettyPrint();
+
+        // 4 - Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("title",equalTo("optio dolor molestias sit"));
 
     }
 
@@ -85,7 +94,22 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseURL {
             response body’sinin null oldugunu test edin
          */
 
+        // 1 - Url hazirla
+
+        specJsonPlace.pathParams("pp1","posts","pp2",50);
+
+        // 2 - Expected Data hazirla
+
+        // 3 - Response'i kaydet
+
+        Response response = given().spec(specJsonPlace).when().delete("/{pp1}/{pp2}");
+
+        // 4 - Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("id",nullValue());
     }
-
-
 }
