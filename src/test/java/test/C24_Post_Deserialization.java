e93e5d1e828ca5da1e6c92f0eb8623477167fdf5
Sezen <aysezen6@gmail.com>
2023-04-12 12:38:16 +0200

    V3

--- src/test/java/test/C24_Post_Deserialization.java
+++ src/test/java/test/C24_Post_Deserialization.java
@@ -1,10 +1,17 @@
 package test;
 
 import baseURL.HerokuAppBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import testData.TestDataHerokuApp;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C24_Post_Deserialization extends HerokuAppBaseUrl {
     /*
@@ -57,6 +64,30 @@ public class C24_Post_Deserialization extends HerokuAppBaseUrl {
 
         HashMap<String,Object> expBody = testDataHerokuApp.expBodyMap();
 
-        // 3 -
+        // 3 - Response 'i kaydet
+
+        Response response = given()
+                                .spec(specHerokuApp)
+                                .contentType(ContentType.JSON)
+                            .when()
+                                .body(reqBody)
+                                .post("/{pp1}");
+
+        response.prettyPrint();
+
+        // 4 - Assertion
+
+        HashMap <String, Object> resMap = response.as(HashMap.class);
+
+        assertEquals(  ((Map)(expBody.get("booking"))).get("firstname")  ,  ((Map)(resMap.get("booking"))).get("firstname")  );
+        assertEquals(  ((Map)(expBody.get("booking"))).get("lastname")   ,  ((Map)(resMap.get("booking"))).get("lastname")   );
+        assertEquals(  ((Map)(expBody.get("booking"))).get("totalprice")   ,  ((Map)(resMap.get("booking"))).get("totalprice")   );
+        assertEquals(  ((Map)(expBody.get("booking"))).get("depositpaid")   ,  ((Map)(resMap.get("booking"))).get("depositpaid")   );
+        assertEquals(  ((Map)(expBody.get("booking"))).get("additionalneeds")   ,  ((Map)(resMap.get("booking"))).get("additionalneeds")   );
+        assertEquals(  ((Map)(((Map)(expBody.get("booking"))).get("bookingdates"))).get("checkin")
+                      ,((Map)(((Map)(resMap.get("booking"))).get("bookingdates"))).get("checkin")    );
+        assertEquals(  ((Map)(((Map)(expBody.get("booking"))).get("bookingdates"))).get("checkout")
+                      ,((Map)(((Map)(resMap.get("booking"))).get("bookingdates"))).get("checkout")    );
+
     }
 }
